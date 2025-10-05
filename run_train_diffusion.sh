a80323a70e475d33c65b4aa40bbd71a5974a0504
sqy <q.y.shi@stu.pku.edu.cn>
2025-10-05 07:52:48 +0000

    lauch diffusion training

--- run_train_diffusion.sh
+++ run_train_diffusion.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 tokenizer_project=tokenizer_training
-tokenizer_exp_name=detokBB-reg0-ch16-p16-g3.0-m0.00.0fix-auxdinov2transformernoisyaligndiffcls
+tokenizer_exp_name=detokBB-ch16-g3.0-m0.7-auxdinov2
 num_register_tokens=0
 
-force_one_d_seq=257
+force_one_d_seq=0
 exp_name=sit_b-${tokenizer_exp_name}
 
 project=gen_model_training
-batch_size=64
+batch_size=128  # nnodes * ngpus * batch_size = 1024
 epochs=100
 
 GPUS_PER_NODE=${GPUS_PER_NODE:-$(nvidia-smi -L 2>/dev/null | wc -l | tr -d ' ')}
@@ -29,14 +29,14 @@ torchrun \
     --project $project --exp_name $exp_name --auto_resume \
     --batch_size $batch_size --epochs $epochs --use_aligned_schedule \
     --pretrained_model_name_or_path "" \
-    --num_register_tokens $num_register_tokens --aux_cls_token --diff_cls_token \
-    --tokenizer detok_BB --use_ema_tokenizer --collect_tokenizer_stats --overwrite_stats \
+    --num_register_tokens $num_register_tokens \
+    --tokenizer detok_BB --use_ema_tokenizer --collect_tokenizer_stats \
     --stats_key $tokenizer_exp_name --stats_cache_path work_dirs/stats.pkl \
-    --load_tokenizer_from work_dirs/$tokenizer_project/$tokenizer_exp_name/checkpoints/latest.pth \
+    --load_tokenizer_from work_dirs/$tokenizer_project/$tokenizer_exp_name/checkpoints/epoch_0199.pth \
     --model SiT_base \
     --force_one_d_seq $force_one_d_seq \
     --num_sampling_steps 250 --cfg 1.3 \
-    --cfg_list 1.1 1.2 1.3 1.4 1.5 1.6 1.7 1.8 \
+    --cfg_list 1.3 1.4 1.5 1.6 1.7 1.8 1.9 2.0 \
     --online_eval --eval_freq 10 \
     --vis_freq 50 --eval_bsz 256 \
     --data_path ./data/imagenet/train
